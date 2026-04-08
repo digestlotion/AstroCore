44a32bb77693cb45676b82f7fdc0ebdee6b66287
digestlotion <keithersuars@gmail.com>
2026-04-08 22:42:38 +0800

    0.0.1 Lotion kuiper slime spawns in kuiper

--- src/main/java/com/astro/core/common/data/AstroEntities.java
+++ src/main/java/com/astro/core/common/data/AstroEntities.java
@@ -3,9 +3,12 @@ package com.astro.core.common.data;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.entity.SpawnPlacements;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.event.entity.SpawnPlacementRegisterEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
@@ -18,23 +21,34 @@ import com.astro.core.common.entity.KuiperSlimeEntity;
 @Mod.EventBusSubscriber(modid = AstroCore.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class AstroEntities {
 
-    public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister
-            .create(ForgeRegistries.ENTITY_TYPES, AstroCore.MOD_ID);
+    public static final DeferredRegister<EntityType<?>> ENTITY_TYPES =
+            DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, AstroCore.MOD_ID);
 
-    public static final RegistryObject<EntityType<KuiperSlimeEntity>> KUIPER_SLIME = ENTITY_TYPES.register(
-            "kuiper_slime",
-            () -> EntityType.Builder.<KuiperSlimeEntity>of(KuiperSlimeEntity::new, MobCategory.MONSTER)
-                    .sized(2.04F, 2.04F)
-                    .build("kuiper_slime"));
+    public static final RegistryObject<EntityType<KuiperSlimeEntity>> KUIPER_SLIME =
+            ENTITY_TYPES.register("kuiper_slime",
+                    () -> EntityType.Builder
+                            .<KuiperSlimeEntity>of(KuiperSlimeEntity::new, MobCategory.MONSTER)
+                            .sized(2.04F, 2.04F).build("kuiper_slime"));
 
     @SubscribeEvent
     public static void registerAttributes(EntityAttributeCreationEvent event) {
-        AttributeSupplier.Builder builder = new AttributeSupplier.Builder(Mob.createMobAttributes().build());
+        AttributeSupplier.Builder builder =
+                new AttributeSupplier.Builder(Mob.createMobAttributes().build());
         builder.add(Attributes.MAX_HEALTH, 1.0);
         builder.add(Attributes.MOVEMENT_SPEED, 0.2);
         builder.add(Attributes.ATTACK_DAMAGE, 0.5);
         event.put(KUIPER_SLIME.get(), builder.build());
     }
 
+    @SubscribeEvent
+    public static void registerSpawnPlacements(SpawnPlacementRegisterEvent event) {
+        event.register(
+            KUIPER_SLIME.get(),
+            SpawnPlacements.Type.ON_GROUND,
+            Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
+            KuiperSlimeEntity::checkSlimeSpawnRules,
+            SpawnPlacementRegisterEvent.Operation.REPLACE
+        );
+    }
     public static void init() {}
 }
--- src/main/java/com/astro/core/common/entity/KuiperSlimeEntity.java
+++ src/main/java/com/astro/core/common/entity/KuiperSlimeEntity.java
@@ -1,12 +1,20 @@
 package com.astro.core.common.entity;
 
 import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.DustParticleOptions;
 import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.SpawnGroupData;
 import net.minecraft.world.entity.monster.Slime;
 import net.minecraft.world.level.Level;
-
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.ServerLevelAccessor;
+import javax.annotation.Nullable;
 import org.joml.Vector3f;
 
 @SuppressWarnings("all")
@@ -20,5 +28,10 @@ public class KuiperSlimeEntity extends Slime {
     @MethodsReturnNonnullByDefault
     public ParticleOptions getParticleType() {
         return new DustParticleOptions(new Vector3f(0.4f, 0.2f, 0.8f), 1.0f);
+    }
+
+    public static boolean checkSlimeSpawnRules(EntityType<KuiperSlimeEntity> type,
+        ServerLevelAccessor level, MobSpawnType spawnType, BlockPos pos, RandomSource random) {
+        return level.getBlockState(pos.below()).isSolidRender(level, pos.below());
     }
 }
